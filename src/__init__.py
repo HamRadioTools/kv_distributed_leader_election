7613a2bed0345d167aa140734a7a169d7731f232
Jonathan González <135473821+ea1het@users.noreply.github.com>
2025-12-14 02:15:43 +0100

    Initialize __init__.py for the package
    Signed-off-by: Jonathan González <135473821+ea1het@users.noreply.github.com>

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# pylint: disable=W0102,E0712,C0103,R0903
+
+"""K/V Distributed Leader Election"""
+
+__updated__ = "2025-12-13 23:41:48"
+__version__ = "0.1.0"
+__all__ = []
